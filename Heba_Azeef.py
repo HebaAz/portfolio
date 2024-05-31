99ee1cf9f1a1426ee711447ad0387009536f8c63
HebaAzf <heba.azeef@gmail.com>
2024-05-30 21:42:36 -0400

    modify layout 2

--- Heba_Azeef.py
+++ Heba_Azeef.py
@@ -5,7 +5,7 @@ st.set_page_config(page_icon=":ribbon:", layout="wide")
 col1, col2 = st.columns([1.0, 1.5])
 
 with col1:
-    st.image("headshot.png")
+    st.image("headshot.png", width=200)
 
 with col2:
     st.title("Heba Azeef")
